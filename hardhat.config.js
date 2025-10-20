f9f4081d01d096136b603a08101da7820e15ee8f
blackyblack <sam.and.tetris@gmail.com>
2025-10-20 16:55:24 +0200

    Use custom solc

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,37 @@
 import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { ProxyAgent, setGlobalDispatcher } from "undici";
+import { execSync } from "child_process";
+import path from "path";
 
-if (process.env.HTTP_PROXY) {
-    console.log("Using proxy for HTTP requests: " + process.env.HTTP_PROXY);
-    const proxyAgent = new ProxyAgent(process.env.HTTP_PROXY);
-    setGlobalDispatcher(proxyAgent);
-}
+// The built-in compiler (auto-downloaded if not yet present) can be overridden by setting SOLC
+// If set, we assume it's a native compiler which matches the required Solidity version.
+/*subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD).setAction(
+    async (args, _hre, runSuper) => {
+        console.log("subtask TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD");
+        if (process.env.SOLC !== undefined) {
+            const versionOutput = execSync(`${process.env.SOLC} --version`, { encoding: 'utf8' }).trim();
+            const longVersion = versionOutput.match(/Version: (.+)/)?.[1] || `${args.solcVersion}+commit.unknown.Linux.g++`;
+            console.log(
+                "Using Solidity compiler set in SOLC:",
+                process.env.SOLC,
+                ", longVersion:",
+                longVersion
+            );
+            return {
+                compilerPath: process.env.SOLC,
+                isSolcJs: true, // false for native compiler
+                version: args.solcVersion,
+                longVersion,
+            };
+        } else {
+            // fall back to the default
+            return runSuper();
+        }
+    }
+);*/
 
 export default {
     solidity: {
+        path: process.env.SOLC,
         version: "0.8.24",
         settings: {
             optimizer: { enabled: true, runs: 200 },
